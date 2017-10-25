852c40ea2676d224fd68ff4eb37bc0f2520f97c2
mninc <manicminer106@gmail.com>
2017-10-25 22:12:43 +0100

    zwoooooooooork
    you broke it again

--- steampy/client.py
+++ steampy/client.py
@@ -151,8 +151,8 @@ class SteamClient:
     @login_required
     def accept_trade_offer(self, trade_offer_id: str) -> dict:
         trade = self.get_trade_offer(trade_offer_id)
-            if trade['trade_offer_state'] is not TradeOfferState.Active:
-                raise ApiException("Invalid trade offer state: {}".format(trade['trade_offer_state']))
+        if trade['trade_offer_state'] is not TradeOfferState.Active:
+            raise ApiException("Invalid trade offer state: {}".format(trade['trade_offer_state']))
         partner = self._fetch_trade_partner_id(trade_offer_id)
         session_id = self._get_session_id()
         accept_url = SteamUrl.COMMUNITY_URL + '/tradeoffer/' + trade_offer_id + '/accept'
