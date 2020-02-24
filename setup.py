061f165032070ec68794a55220a759b53d8197bd
Michał Bukowski <gigibukson@gmail.com>
2020-02-24 22:44:34 +0100

    Update version to 0.62

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 if not sys.version_info[0] == 3 and sys.version_info[1] < 5:
     sys.exit('Python < 3.5 is not supported')
 
-version = '0.60'
+version = '0.62'
 
 setup(
     name='steampy',
