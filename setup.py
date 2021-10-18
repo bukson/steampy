2f49918d1560b0c8a94e43afc092fe3b23331ee6
bukson <michal.bukowski@buksoft.pl>
2021-10-18 10:02:31 +0200

    Update setup.py to version 0.75

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 if not sys.version_info[0] == 3 and sys.version_info[1] < 5:
     sys.exit('Python < 3.5 is not supported')
 
-version = '0.74'
+version = '0.75'
 
 setup(
     name='steampy',
