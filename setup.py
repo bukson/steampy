5436dc56789a4bae48b125f5d3d1ac7e0da547bf
bukson <michal.bukowski@buksoft.pl>
2023-09-14 15:54:57 +0200

    Update setup.py to version 0.90

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 if not sys.version_info[0] == 3 and sys.version_info[1] < 5:
     sys.exit('Python < 3.5 is not supported')
 
-version = '0.80'
+version = '0.90'
 
 setup(
     name='steampy',
