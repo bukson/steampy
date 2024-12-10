474a7675a3ad719375248ec33f454d7b456faa47
Wok <woctezuma@users.noreply.github.com>
2024-12-11 00:22:35 +0100

    Minor formatting

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ version = '1.1.2'
 
 setup(
     name='steampy',
-    packages=['steampy', 'test', 'examples' ],
+    packages=['steampy', 'test', 'examples'],
     version=version,
     description='A Steam lib for trade automation',
     author='Michał Bukowski',
@@ -17,7 +17,7 @@ setup(
     license='MIT',
     url='https://github.com/bukson/steampy',
     download_url='https://github.com/bukson/steampy/tarball/' + version,
-    keywords=['steam', 'trade' ],
+    keywords=['steam', 'trade'],
     classifiers=[],
     install_requires=[
         "requests",
